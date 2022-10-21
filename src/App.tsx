6d60220e19a13805131b0797d3118c1764d528ed
EvgHrn <evg.hrn@gmail.com>
2022-10-21 12:48:30 +0400

    Socket init

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { io } from "socket.io-client";
+
+const socket = io("https://timesheets.space");
+
+socket.on("connect", () => {
+  console.log(`Socket connected with id ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+  console.log(`Socket disconnected with id ${socket.id}`);
+});
+
+socket.on("print", (...args) => {
+  console.log(`Socket print event with args ${args}`);
+});
+
+socket.onAny((eventName, ...args) => {
+  console.log(`Socket event ${eventName} with args ${args}`);
+});
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloaddddddd.
         </p>
         <a
           className="App-link"
